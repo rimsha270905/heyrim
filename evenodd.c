b39bc9eabb6dc6de50ce754591a20a8c93476779
rimsha270905 <rimzrose27@gmail.com>
2025-04-03 10:15:52 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,19 @@
+#even or ODD
+
+#include<stdio.h>
+
+#include<conio.h>
+
+void main()
+{
+int num;
+Printf("enter number");
+scanf("%d",&no);
+if(num%2==0)
+  printf("Even");
+else
+  printf("odd");
+getch()
+}
+
+
